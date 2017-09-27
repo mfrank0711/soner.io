e6e633acadad074cbb4cdfeca3b00ff91b3b7002
Soner Kalayci <soner.kalayci@zoi.de>
2017-09-27 16:56:49 +0200

    Trying to get travis to work

--- script/merge.py
+++ script/merge.py
@@ -3,35 +3,35 @@ def insertContent(htmlFile):
         for content in newLine:
             fout.write(content)
 
-with open("../main.html", "rt") as fin:
-    with open("../index.html", "wt") as fout:
+with open("main.html", "rt") as fin:
+    with open("index.html", "wt") as fout:
         for line in fin:
             fout.write(line);
             if ('<div id="aws_mysql">' in line):
-                insertContent("../aws_mysql.html")
+                insertContent("aws_mysql.html")
             
             elif ('<div id="aws_mssql">' in line):
-                insertContent("../aws_mssql.html")
+                insertContent("aws_mssql.html")
                         
             elif ('<div id="aws_nosql">' in line):
-                insertContent("../aws_nosql.html")
+                insertContent("aws_nosql.html")
                 
             elif ('<div id="aws_datawarehouse">' in line):
-                insertContent("../aws_datawarehouse.html")
+                insertContent("aws_datawarehouse.html")
                 
             elif ('<div id="aws_elasticache">' in line):
-                insertContent("../aws_elasticache.html")
+                insertContent("aws_elasticache.html")
                 
                 
             elif ('<div id="ms_mysql">' in line):
-                insertContent("../ms_mysql.html")
+                insertContent("ms_mysql.html")
                 
             elif ('<div id="ms_nosql">' in line):
-                insertContent("../ms_nosql.html")
+                insertContent("ms_nosql.html")
                 
                 
             elif ('<div id="gcp_mysql">' in line):
-                insertContent("../gcp_mysql.html")
+                insertContent("gcp_mysql.html")
                 
             elif ('<div id="gcp_nosql">' in line):
-                insertContent("../gcp_nosql.html")
+                insertContent("gcp_nosql.html")
